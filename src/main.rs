e299219df55d85929b104620bc58cf626905530f
SalimBXL <el.salim.salim@gmail.com>
2024-11-24 14:05:16 +0100

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
-    println!("Salut les codeurs! avec println");
-    print!("Salut les amis! avec print");
-    eprintln!("Hello les bugs! eprintln");
+    let name: &str = "Salim";
+    let age: i32 = 48;
+    println!("Salut, moi c'est {name} et j'ai {age} ans.");
+    println!("{:>10}", "Coucou");
+    println!("{:>25}", "Bonjour");
+    println!("{:>15}", "Ok");
 }
